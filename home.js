d4ea610f8d65ec7423eb71f7ea59a54d32c55529
ADM3M <fly.swift499@gmail.com>
2022-03-29 10:51:05 +0300

    Удалён бесполезный файл. Добавлен файл с js кодом.

---
+++ home.js
@@ -0,0 +1,16 @@
+const btn_hide = document.getElementById('btn-hide');
+const btn_appear = document.getElementById('btn-appear');
+const nav_block = document.getElementById('nav-block');
+const collapsed_nav = document.getElementById('collapsed-nav-block');
+
+btn_hide.addEventListener('click', function() {
+    console.log('hide');
+    nav_block.classList.add('hide');
+    collapsed_nav.classList.remove('hide');
+})
+
+btn_appear.addEventListener('click', function() {
+    console.log('appear');
+    collapsed_nav.classList.add('hide');
+    nav_block.classList.remove('hide');
+})
