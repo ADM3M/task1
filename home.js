edac7e5e12007f521eac8fdf8409b3a259addeb6
ADM3M <fly.swift499@gmail.com>
2022-03-29 14:06:07 +0300

    починил значения

--- home.js
+++ home.js
@@ -19,5 +19,5 @@ const links = document.getElementsByTagName('a');
 
 for (let i=0; i<links.length; i++)
 {
-    links[i].setAttribute('href', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    links[i].setAttribute('href', 'http://bit.ly/3uBFWs5');
 }
